d3af577929a75e410030f66d26da1b250e76eb7e
Kirill <kirillcarev745@gmail.com>
2023-08-30 19:43:53 +1000

    bruh

--- Resources/Reforms/Reform.gd
+++ Resources/Reforms/Reform.gd
@@ -5,4 +5,4 @@ class_name Reform
 @export var levels: Array[ReformLevel] = []
 @export var reform_name: String = ""
 
-@export_enum("soc_reform", "pol_reform") var reform_type = "soc_reform"
+@export_enum("soc_reform", "pol_reform_") var reform_type = "soc_reform"
